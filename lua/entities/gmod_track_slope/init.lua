0d16774f6420346a577b32ed2987108e73082ae6
klusandr <klusandr@mail.ru>
2022-12-03 09:02:07 +0300

    prepared slopes

--- lua/entities/gmod_track_slope/init.lua
+++ lua/entities/gmod_track_slope/init.lua
@@ -1,3 +1,16 @@
+-----------------------------------------------------------------------
+--                          Творческое объединение "MetroPack"
+--	Скрипт написан в 2021 году для карты gm_metro_minsk_1984.
+--  В 2022 выделен в отдельный аддон для дополнения Metrostroi.
+--	Аддон добавляет в игру знаки уклонов.
+--  Авторы: klusandr, ilya2000 (Илья Прокофьев) Classic Metrostroi Project
+--	Steam klusandr: https://steamcommunity.com/id/andr47/
+--	VK klusandr:	https://vk.com/andreyklysevich
+--  Дополнительная информация в файле lua/licence.lua
+-----------------------------------------------------------------------
+
+if (not Metrostroi) then return end
+
 AddCSLuaFile("cl_init.lua")
 AddCSLuaFile("shared.lua")
 include("shared.lua")
--- lua/metrostroi/sv_slope.lua
+++ lua/metrostroi/sv_slope.lua
@@ -1,32 +1,89 @@
-local function getFile(path,name,id)
+-----------------------------------------------------------------------
+--                          Творческое объединение "MetroPack"
+--	Скрипт написан в 2021 году для карты gm_metro_minsk_1984.
+--  В 2022 выделен в отдельный аддон для дополнения Metrostroi.
+--	Аддон добавляет в игру знаки уклонов.
+--  Авторы: klusandr, ilya2000 (Илья Прокофьев) Classic Metrostroi Project
+--	Steam klusandr: https://steamcommunity.com/id/andr47/
+--	VK klusandr:	https://vk.com/andreyklysevich
+--  Дополнительная информация в файле lua/licence.lua
+-----------------------------------------------------------------------
+
+-- Logger --
+
+local function log(message, category)
+    if (Metrostroi.Logger) then
+        Metrostroi.Logger.Log(message, category, "Slopes")
+    end   
+end
+
+local function logError(message)
+    log(message, "Error")
+end
+
+local function logInfo(message)
+    log(message, "Info")
+end
+
+local filePath = "metrostroi_data/slope_"..game.GetMap()..".txt";
+
+----
+
+-- Check exists file.
+local function checkFile(path)
+    return file.Exists(path, "DATA") or file.Exists(path, "LUA")
+end
+
+-- Load data from the file.
+-- (file) - File name.
+-- RETURN - JSON data from file.
+local function loadFile(path)
     local data,found
-    if file.Exists(Format(path..".txt",name),"DATA") then
-        data= util.JSONToTable(file.Read(Format(path..".txt",name),"DATA"))
+    if file.Exists(path, "DATA") then
+        data = util.JSONToTable(file.Read(path, "DATA"))
         found = true
     end
-    if not data and file.Exists(Format(path..".lua",name),"LUA") then
-        data= util.JSONToTable(file.Read(Format(path..".lua",name),"LUA"))
+    if not data and file.Exists(path, "LUA") then
+        data = util.JSONToTable(file.Read(path, "LUA"))
         found = true
     end
     if not found then
+        logError("Configuration file not found. Path: '"..path.."'")
         return
     elseif not data then
+        logError("Configuration parse JSON error")
         return
     end
     return data
 end
 
-local function loadSlopes(name, keep)
-    name = name or game.GetMap()
+-- Save data in the file.
+-- (path) - Full path to file.
+-- (data) - Data written to file.
+local function saveFile(path, data)
+    file.Write(path, data)
+end
 
-    local slopes_ents = ents.FindByClass("gmod_track_slope")
-    for k,v in pairs(slopes_ents) do SafeRemoveEntity(v) end
+-- Remove the file.
+-- (file) - File name.
+local function removeFile(path)
+    file.Delete(path)
+end
 
-    if keep then return end
-    local slopes = getFile("metrostroi_data/slope_%s",name,"Slope")
-    if not slopes then return end
+Metrostroi.Slope = {}
 
-    for k,v in pairs(slopes) do
+-- Load slopes.
+function Metrostroi.Slope.Load()
+    if (not checkFile(filePath)) then return end
+
+    for _, ent in pairs(ents.FindByClass("gmod_track_slope")) do
+        SafeRemoveEntity(ent) 
+    end
+
+    local data = loadFile(filePath)
+    if not data then return end
+
+    for k,v in pairs(data) do
         local ent = ents.Create("gmod_track_slope")
         if IsValid(ent) then
             ent:SetPos(v.Pos)
@@ -43,52 +100,51 @@ local function loadSlopes(name, keep)
             ent:Spawn()
         end
     end
-    
-end
 
-hook.Add("Initialize", "Metrostroi_MapSlopeInitialize", function()
-    timer.Simple(2.0, loadSlopes)
-end)
+    logInfo("Pickets is loaded")
+end 
 
-
-timer.Simple(1, function()                  --Задержка после загрузки игры
-
-local m_save = Metrostroi.Save
-function Metrostroi.Save(name)
-    m_save(name)
-
-    if not file.Exists("metrostroi_data","DATA") then
-        file.CreateDir("metrostroi_data")
-    end
-    name = name or game.GetMap()
-
-    -- Format signs, signal, switch data
+-- Save slopes.
+function Metrostroi.Slope.Save()
     local slopes = {}
-    local slopes_ents = ents.FindByClass("gmod_track_slope")
-    
-    for k,v in pairs(slopes_ents) do
+
+    for _, ent in pairs(ents.FindByClass("gmod_track_slope")) do
         table.insert(slopes, {
-            Pos = v:GetPos(),
-            Angles = v:GetAngles(),
-            Type = (v.Type ~= 1) and v.Type or nil,
-            YOffset = (v.YOffset ~= 0) and v.YOffset or nil,
-            ZOffset = (v.ZOffset ~= 0) and v.ZOffset or nil,
-            PAngle = (v.PAngle ~= 0) and v.PAngle or nil,
-            YAngle = (v.YAngle ~= 0) and v.YAngle or nil,
-            Value_u = v.Value_u,
-            Length = v.Length,w
+            Pos = ent:GetPos(),
+            Angles = ent:GetAngles(),
+            Type = (ent.Type ~= 1) and ent.Type or nil,
+            YOffset = (ent.YOffset ~= 0) and ent.YOffset or nil,
+            ZOffset = (ent.ZOffset ~= 0) and ent.ZOffset or nil,
+            PAngle = (ent.PAngle ~= 0) and ent.PAngle or nil,
+            YAngle = (ent.YAngle ~= 0) and ent.YAngle or nil,
+            Value_u = ent.Value_u,
+            Length = ent.Length,w
         })
     end
-    local data = util.TableToJSON(slopes, true)
-    file.Write(string.format("metrostroi_data/slope_%s.txt", name), data)
+
+    print("Metrostroi: Saving slopes definition...")
+
+    if (not table.IsEmpty(slopes)) then
+        saveFile(filePath, util.TableToJSON(slopes, true))
+        print("Saved to "..filePath)
+    else
+        removeFile(filePath);
+        print("File remove "..filePath)
+    end
 end
 
-local m_load = Metrostroi.Load
-function Metrostroi.Load(name,keep_signs)
-    m_load(name,keep_signs)
-    loadSlopes(name,keep_signs)
-end
+timer.Simple(1, function()                  --Задержка после загрузки игры
+    local m_save = Metrostroi.Save
+    function Metrostroi.Save(name)
+        m_save(name)
+        Metrostroi.Slope.Save()
+    end
 
+    local m_load = Metrostroi.Load
+    function Metrostroi.Load(name, keep_signs)
+        m_load(name,keep_signs)
+        Metrostroi.Slope.Load()
+    end
 end)                                        --Окончание тела функции с задержкой
 
-Metrostroi.Slope = {}
+timer.Simple(2, Metrostroi.Slope.Load)
